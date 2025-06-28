c27346483fb9deb71f2a2c36b76978050d08eac2
pranjal023 <123534075+pranjal023@users.noreply.github.com>
2025-06-28 04:45:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 
 const express = require('express');
 const app = express();
+let PORT= 3000
+app.use(express.json())
+
+
+app.get('/orders', (req, res) => {
+  res.send('Here is the list of all orders.');
+});
+
+
+app.post('/orders', (req, res) => {
+  res.send('A new order has been created.');
+});
+
+
+app.get('/users', (req, res) => {
+  res.send('Here is the list of all users.');
+});
+
+
+app.post('/users', (req, res) => {
+  res.send('A new user has been added.');
+});
 
 const addUser = (req, res, next) => {
   req.user = 'Guest';
@@ -11,7 +33,7 @@ const addUser = (req, res, next) => {
 app.get('/welcome', addUser, (req, res) => {
   res.send(`<h1>Welcome, ${req.user}!</h1>`);
 });
-const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
 });
